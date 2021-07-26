de42cefb00d1b0b77d7822240769082e233063a7
dohyung97022 <dohyung97022@gmail.com>
2021-07-26 11:55:46 +0900

    feat : spring 설치

---
+++ src/main/java/com/kim/socialloginspring/SocialLoginSpringApplication.java
@@ -0,0 +1,13 @@
+package com.kim.socialloginspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SocialLoginSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SocialLoginSpringApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/kim/socialloginspring/SocialLoginSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kim.socialloginspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SocialLoginSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
